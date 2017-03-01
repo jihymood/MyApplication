c654934b6a4bda19e811fea2e7c9ea178d4439d4
jihymood <huangjihy@163.com>
2017-03-01 17:28:02 +0800

    ormlite

--- volley/src/main/java/com/example/volley/ImageLruCache.java
+++ volley/src/main/java/com/example/volley/ImageLruCache.java
@@ -8,22 +8,15 @@ import com.android.volley.toolbox.ImageLoader;
 /**
  * Created by huangjh on 2017/3/1 0001 17:08
  * Email：huangjihy@163.com
- *
  */
+
 /** Lru算法的L1缓存实现类. */
 public class ImageLruCache implements ImageLoader.ImageCache {
 
     private LruCache<String, Bitmap> mLruCache;
 
     public ImageLruCache() {
-        this((int) Runtime.getRuntime().maxMemory() / 8);
-    }
-
-    public ImageLruCache(final int cacheSize) {
-        createLruCache(cacheSize);
-    }
-
-    private void createLruCache(final int cacheSize) {
+        int cacheSize = ((int) Runtime.getRuntime().maxMemory() / 8);
         mLruCache = new LruCache<String, Bitmap>(cacheSize) {
             @Override
             protected int sizeOf(String key, Bitmap value) {
@@ -31,6 +24,19 @@ public class ImageLruCache implements ImageLoader.ImageCache {
             }
         };
     }
+
+//    public ImageLruCache(final int cacheSize) {
+//        createLruCache(cacheSize);
+//    }
+
+//    private void createLruCache(final int cacheSize) {
+//        mLruCache = new LruCache<String, Bitmap>(cacheSize) {
+//            @Override
+//            protected int sizeOf(String key, Bitmap value) {
+//                return value.getRowBytes() * value.getHeight();
+//            }
+//        };
+//    }
 
     @Override
     public Bitmap getBitmap(String s) {
