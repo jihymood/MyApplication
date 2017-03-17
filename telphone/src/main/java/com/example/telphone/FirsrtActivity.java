38b7fa2b2ee7178f092d07610d56243aae652163
jihymood <huangjihy@163.com>
2017-03-17 09:18:48 +0800

    不知道

---
+++ telphone/src/main/java/com/example/telphone/FirsrtActivity.java
@@ -0,0 +1,13 @@
+package com.example.telphone;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FirsrtActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_firsrt);
+    }
+}
---
+++ telphone/src/main/java/com/example/telphone/PhoneUtils.java
@@ -0,0 +1,8 @@
+package com.example.telphone;
+
+/**
+ * Created by huangjh on 2017/3/15 0015 14:07
+ * Email：huangjihy@163.com
+ */
+public class PhoneUtils {
+}
