a1e4ad8d8a4750952cc92734afdd732da01deeaa
jihymood <huangjihy@163.com>
2017-05-26 09:42:05 +0800

    mvp

---
+++ mvptest/src/main/java/com/example/mvptest/login/view/FirstActivity.java
@@ -0,0 +1,37 @@
+package com.example.mvptest.login.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.mvptest.R;
+import com.example.mvptest.login.model.IUser;
+import com.example.mvptest.login.model.UserImpl;
+import com.example.mvptest.login.presenter.IPresenter;
+import com.example.mvptest.login.presenter.PresenterImpl;
+
+public class FirstActivity extends AppCompatActivity implements IView{
+
+    private IPresenter iPresenter;
+    private IUser iUser;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+
+        iUser = new UserImpl("傻逼", "123");
+        iPresenter = new PresenterImpl(iUser, this);
+        iPresenter.login();
+    }
+
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public void login(String name, String pwd) {
+
+    }
+}
